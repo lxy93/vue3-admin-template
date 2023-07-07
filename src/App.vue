83b18950367c485d9414bfa8dc3d1c9d53fe3215
lxy <1406062098@qq.com>
2023-07-07 15:05:54 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="box">
+    <h2>app组件</h2>
+    {{ str }}
+  </div>
+</template>
+<script setup lang="ts">
+const str = "xxx";
+</script>
+<style lang="scss" scoped>
+.box {
+  width: 100px;
+  height: 100px;
+  background: #fff;
+  h2 {
+    color: red;
+  }
+}
+</style>
